99e6704e5a9c2fd2fb3106a905e76097b83985fd
darrenliuwei <darrenliuwei@gmail.com>
2021-03-29 22:08:34 +0800

    百度快捷搜索第一版

---
+++ js/background.js
@@ -0,0 +1,8 @@
+chrome.contextMenus.create({
+  type: 'normal',
+  title: '百度一下',
+  contexts: ['selection'],
+  onclick: (info) => {
+    window.open('https://www.baidu.com/s?wd=' + info.selectionText)
+  },
+})
