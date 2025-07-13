2ef39fb9da6fbf9e7f86769b9b5b0aa6f6adf934
Darren <darrenliuwei@gmail.com>
2025-07-13 20:37:24 +0800

    change manifest_version

--- js/background.js
+++ js/background.js
@@ -1,8 +1,19 @@
-chrome.contextMenus.create({
-  type: 'normal',
-  title: '百度一下',
-  contexts: ['selection'],
-  onclick: (info) => {
-    window.open('https://www.baidu.com/s?wd=' + info.selectionText.replaceAll('&', '%26'))
-  },
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    type: 'normal',
+    id: 'myUniqueMenuItemId',
+    title: '百度一下',
+    contexts: ['selection'],
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'myUniqueMenuItemId') {
+    if (info.selectionText) {
+      chrome.tabs.create({
+        url: `https://www.baidu.com/s?wd=` + info.selectionText.replaceAll('&', '%26'),
+        active: true,
+      })
+    }
+  }
 })
